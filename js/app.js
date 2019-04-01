6e696077e56eef9a5e437a26d2f3fadca93fa2cf
Meerds <diogofreitascastro@gmail.com>
2019-04-01 09:57:59 +0100

    jumpLinks scroll

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-$(document).foundation()
+$(document).ready(function () {
+    // Foundation
+    $(document).foundation();
+
+
+$('a.jumpLinks').click(function(){
+        $('html, body').animate({
+            scrollTop: $( $(this).attr('href') ).offset().top
+        }, 500);
+        return false;
+    });
+});
